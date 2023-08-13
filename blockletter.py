e1d6db58ea2fd52523d6404888b831d45a41a8f1
Wgang231 <103540449+Wgang231@users.noreply.github.com>
2023-08-13 18:49:46 +0200

    Create blockletter.py

---
+++ blockletter.py
@@ -0,0 +1,23 @@
+# Dubard Jones
+# I have 37inches verticals
+
+name = """ 
+      D D D D
+      D       D     
+      D       D     
+      D       D     
+      D       D     
+      D       D  
+      D D D D 
+
+       J J J J J
+          J
+          J
+      J   J
+      J   J
+        J J      
+
+"""
+
+
+print(name)
